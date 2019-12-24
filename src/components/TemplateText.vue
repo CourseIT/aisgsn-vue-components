52b0ec6df2201d4d77ec6b61a8db6349dbf97c52
temirka13 <temirka13@mail.ru>
2019-12-24 15:30:20 +0300

    fix Textarea

---
+++ src/components/TemplateText.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="template-block">
+    <div class="w90" @click="selectText">
+      <p>{{text}}</p>
+    </div>
+    <div class="icons-block">
+      <Icon icon="" class="icon mb10"/>
+      <Icon icon="" class="icon"/>
+      <Icon icon="" class="icon"/>
+    </div>
+  </div>
+</template>
+
+<script>
+const Icon = () => import('./Icon')
+export default {
+  components: {
+    Icon
+  },
+  data: () => ({
+    text: 'Шаблонный текст 1 …'
+  }),
+  methods:{
+    selectText() {
+      this.$emit('selectText', this.text)
+    },
+  }
+}
+</script>
+
+<style scoped>
+.w90{
+  width: 90%;
+}
+.mb10{
+  margin-bottom: 10px;
+}
+.template-block {
+  display: flex;
+  justify-content: space-between;
+  background: #fff;
+  padding: 4px 8px;
+  border-radius: 4px;
+  width: 100%;
+  overflow: auto;  
+  margin-bottom: 6px;
+  transition: all 0.2s ease-out;
+  cursor: pointer;
+}
+.template-block:hover{
+  box-shadow: 0 7px 10px 0 rgba(0, 0, 0, 0.22);
+}
+.template-block:hover p {
+  color: #fb6229;
+}
+.icon {
+  cursor: pointer;
+  font-family: var(--font-awesome-5-pro-light) !important;
+}
+</style>
--- src/components/TextareaWithIcon.vue
+++ src/components/TextareaWithIcon.vue
@@ -3,22 +3,44 @@
     <p class="label">{{label}}</p>
     <div class="df">
       <v-textarea
+        v-model="text"
         :placeholder="placeholder"
         :flat=true
         solo
         auto-grow
       ></v-textarea>
-			<Icon :icon="icon" class="icon"/>
+      <div class="icon" @click="template_show = true"><Icon :icon="icon" class="icon-block"/></div>
+      <div class="templates-block" v-if="template_show">
+        <TemplateText @selectText="selectText" v-for="(text, index) in texts" :key="index" />
+      </div>
 		</div>
+    <div class="close-block" v-if="template_show" @click="template_show = false"></div>
   </div>
 </template>
 
 <script>
 const Icon = () => import('./Icon')
+const TemplateText = () => import('./TemplateText')
 export default {
 	props: ['placeholder', 'value', 'icon', 'label'],
   components: {
-    Icon
+    Icon,
+    TemplateText
+  },
+  data: () =>({
+    text: '',
+    template_show: false,
+    texts:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+  }),
+  watch: {
+    text(){
+      this.template_show = false
+    }
+  },
+  methods: {
+    selectText(text) {
+      this.text = text
+    }
   }
 }
 </script>
@@ -42,6 +64,7 @@ export default {
 .input-block {
   margin-bottom: -15px;
 }
+
 .icon {
   cursor: pointer;
   height: 40px;
@@ -51,5 +74,22 @@ export default {
   width: 5%;
   text-align: center;
 }
-
+.close-block {
+  position: fixed;
+  z-index: 9;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+}
+.templates-block {
+  position: absolute;
+  z-index: 99;
+  left: 66%;
+  max-width: 450px;
+  min-width: 400px;
+  overflow: auto;
+  max-height: 500px;
+  padding-right: 30px;
+}
 </style>
